98ff29215ea54ce1b822d8d65e14d18d91bf8688
Tumelo Mogono <tumelomogono@gmail.com>
2024-05-07 11:33:05 +0200

    completed the second user story, to not divide if no input values are provided

--- scripts.js
+++ scripts.js
@@ -5,11 +5,19 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+
+  console.log(dividend);
+  console.log(divider)
   
   let divideResult = dividend / divider;
 
   if ( divideResult % 1 !== 0) {
     divideResult = Math.floor(divideResult);
+  }
+
+  if ( dividend === "" || divider === "") {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    return
   }
 
   result.innerText = divideResult;
